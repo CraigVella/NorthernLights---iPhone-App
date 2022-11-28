fae0274f4331eb82d5a2a1c35e07697c65018767
Craig Vella <CraigVella@gmail.com>
2022-11-28 16:09:59 -0500

    Initial Commit

---
+++ NorthernLights/NorthernLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorthernLightsApp.swift
+//  NorthernLights
+//
+//  Created by Craig Vella on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct NorthernLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
